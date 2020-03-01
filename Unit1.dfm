45444c6e9843c2019236b12995bc96975653fab1
OhotnikovV <vitaliyohotnikov24@gmail.com>
2020-03-01 19:07:26 +0700

    Привел в порядок код

--- Unit1.dfm
+++ Unit1.dfm
@@ -3,7 +3,7 @@ object Form1: TForm1
   Top = 0
   Caption = #1050#1083#1080#1077#1085#1090
   ClientHeight = 257
-  ClientWidth = 504
+  ClientWidth = 481
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -16,7 +16,7 @@ object Form1: TForm1
   TextHeight = 13
   object Button1: TButton
     Left = 16
-    Top = 152
+    Top = 207
     Width = 75
     Height = 25
     Caption = #1054#1090#1087#1088#1072#1074#1080#1090#1100
@@ -26,18 +26,19 @@ object Form1: TForm1
   object StatusBar1: TStatusBar
     Left = 0
     Top = 238
-    Width = 504
+    Width = 481
     Height = 19
     Panels = <
       item
         Width = 50
       end>
+    ExplicitWidth = 504
   end
   object Memo1: TMemo
-    Left = 240
-    Top = 48
-    Width = 209
-    Height = 129
+    Left = 8
+    Top = 8
+    Width = 465
+    Height = 169
     TabOrder = 2
   end
   object ClientSocket1: TClientSocket
@@ -45,15 +46,15 @@ object Form1: TForm1
     ClientType = ctNonBlocking
     Port = 0
     OnConnect = ClientSocket1Connect
-    Left = 96
-    Top = 32
+    Left = 352
+    Top = 192
   end
   object ServerSocket1: TServerSocket
     Active = False
     Port = 0
     ServerType = stNonBlocking
     OnClientRead = ServerSocket1ClientRead
-    Left = 176
-    Top = 32
+    Left = 424
+    Top = 192
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -33,36 +33,37 @@ implementation
 
 {$R *.dfm}
 
-
-
+// отправить xml сообщение
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  ClientSocket1.Socket.SendText('Привет');
   ClientSocket1.Socket.SendText('<computers><ip>192.168.100.3</ip></computers>');
-
-{  //включаем сервер  для обратной связи
-  ServerSocket1.Port := 64000;
-  ServerSocket1.Active := True; // активируем сервер
-  ServerSocket1.Open; // запускаем       }
+  Memo1.Lines.Add('Сообщение отправлено');
 end;
 
-// вывод и отправка сообщения о подключении
+// Процедура - при коннекте
 procedure TForm1.ClientSocket1Connect(Sender: TObject;
   Socket: TCustomWinSocket);
 begin
   if ClientSocket1.Active then
-    Statusbar1.Panels.Items[0].Text:='Connection to ServerSocket1 192.168.100.3';
+    Statusbar1.Panels.Items[0].Text:='Connection to ServerSocket1 192.168.100.3'; // вывод сообщения о подключении
 end;
 
+// Процедура - при создании формы
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-// активировать клиент, подключится к серверу
+  // активировать клиент, подключится к серверу
   ClientSocket1.Address := '192.168.100.3'; // IP адрес сервера
   ClientSocket1.Port := 65000; // его порт
   ClientSocket1.Active := True; // активируем клиента
   ClientSocket1.Open; // запускаем
+
+  //включаем сервер  для обратной связи
+  {ServerSocket1.Port := 64000;
+  ServerSocket1.Active := True; // активируем сервер
+  ServerSocket1.Open; // запускаем       }
 end;
 
+// Процедура - клиент установил сокетное соединение и ждет ответа сервера
 procedure TForm1.ServerSocket1ClientRead(Sender: TObject;
   Socket: TCustomWinSocket);
 begin
