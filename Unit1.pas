2596a494ac027dbedc7030362c4dda6c1fae69e8
OhotnikovV <vitaliyohotnikov24@gmail.com>
2020-03-07 19:50:16 +0700

    Добавлена функция определения Мак адреса, отправляет сообщение при конекте и закрытии программы

--- Unit1.pas
+++ Unit1.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Win.ScktComp,
-  Vcl.ComCtrls, Vcl.ExtCtrls;
+  Vcl.ComCtrls, Vcl.ExtCtrls, NB30;
 
 type
   TForm1 = class(TForm)
@@ -27,8 +27,11 @@ type
     procedure Button2Click(Sender: TObject);
     procedure TrayIcon1DblClick(Sender: TObject);
     procedure ClientSocket1Read(Sender: TObject; Socket: TCustomWinSocket);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     function GetComputerNetName: string;
+    function GetAdapterInfo(Lana: Char): string;
+    function GetMACAddress: string;
     { Private declarations }
   public
     { Public declarations }
@@ -56,7 +59,7 @@ begin
   Statusbar1.Panels.Items[0].Text:='Connection to '+Socket.RemoteAddress;
 
   // отправить xml сообщение с даннами
-  ClientSocket1.Socket.SendText('<computers><NameComputer>'+GetComputerNetName+'</NameComputer><MAC_address>B0-6E-BF-5C-E2-10</MAC_address></computers>');
+  ClientSocket1.Socket.SendText('<computers><NameComputer>'+GetComputerNetName+'</NameComputer><MAC_address>'+GetMACAddress+'</MAC_address></computers>');
   Memo1.Lines.Add('Сообщение отправлено');
 end;
 
@@ -74,9 +77,16 @@ begin
   ErrorCode:=0;
 end;
 
+// Процедура -  при передаче сообщения от сервера клиенту
 procedure TForm1.ClientSocket1Read(Sender: TObject; Socket: TCustomWinSocket);
 begin
   Memo1.Lines.Add(Socket.ReceiveText);
+end;
+
+// Процедура - при закрытии формы
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  ClientSocket1.Socket.SendText('<computers><NameComputer>'+GetComputerNetName+'</NameComputer><MAC_address>'+GetMACAddress+'</MAC_address></computers>');
 end;
 
 // Процедура - при создании формы
@@ -87,8 +97,6 @@ begin
   ClientSocket1.Port := 65000; // его порт
   ClientSocket1.Active := True; // активируем клиента}
   ClientSocket1.Open; // запускаем
-
-  memo1.Lines.Add(GetComputerNetName); //вывод имени
 
   //включаем сервер  для обратной связи
   {ServerSocket1.Port := 64000;
@@ -113,7 +121,7 @@ begin
   TrayIcon1.Visible:=False;
 end;
 
-// функция определения имени компьютера
+// Функция определения имени компьютера
 function TForm1.GetComputerNetName: string;
 var
   buffer: array[0..255] of char;
@@ -124,6 +132,60 @@ begin
     Result := buffer
   else
     Result := ''
+end;
+
+// Функция получения информации от сетевого адаптера
+function TForm1.GetAdapterInfo(Lana: Char): string;
+var
+  Adapter: TAdapterStatus;
+  NCB: TNCB;
+begin
+  FillChar(NCB, SizeOf(NCB), 0);
+  NCB.ncb_command := Char(NCBRESET);
+  NCB.ncb_lana_num := AnsiChar(Lana);
+  if Netbios(@NCB) <> Char(NRC_GOODRET) then
+  begin
+    Result := 'Адрес не известен';
+    Exit;
+  end;
+
+  FillChar(NCB, SizeOf(NCB), 0);
+  NCB.ncb_command := Char(NCBASTAT);
+  NCB.ncb_lana_num := AnsiChar(Lana);
+  NCB.ncb_callname := '*';
+
+  FillChar(Adapter, SizeOf(Adapter), 0);
+  NCB.ncb_buffer := @Adapter;
+  NCB.ncb_length := SizeOf(Adapter);
+  if Netbios(@NCB) <> Char(NRC_GOODRET) then
+  begin
+    Result := 'Адрес не известен';
+    Exit;
+  end;
+  Result :=
+  IntToHex(Byte(Adapter.adapter_address[0]), 2) + '-' +
+  IntToHex(Byte(Adapter.adapter_address[1]), 2) + '-' +
+  IntToHex(Byte(Adapter.adapter_address[2]), 2) + '-' +
+  IntToHex(Byte(Adapter.adapter_address[3]), 2) + '-' +
+  IntToHex(Byte(Adapter.adapter_address[4]), 2) + '-' +
+  IntToHex(Byte(Adapter.adapter_address[5]), 2);
+end;
+
+// Функция определения МАК адреса адаптера
+function TForm1.GetMACAddress: string;
+var
+  AdapterList: TLanaEnum;
+  NCB: TNCB;
+begin
+  FillChar(NCB, SizeOf(NCB), 0);
+  NCB.ncb_command := Char(NCBENUM);
+  NCB.ncb_buffer := @AdapterList;
+  NCB.ncb_length := SizeOf(AdapterList);
+  Netbios(@NCB);
+  if Byte(AdapterList.length) > 0 then
+    Result := GetAdapterInfo(Char(AdapterList.lana[0]))
+  else
+    Result := 'Адрес не известен';
 end;
 
 end.
